261e3101b1c796cd382cea17935807f1863141ff
Nawaz2000 <mail2nawaz2000@gmail.com>
2021-09-19 20:14:35 +0530

    register, login, myorders view resolved

---
+++ src/main/java/com/nawaz2000/petshop/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.nawaz2000.petshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+}
